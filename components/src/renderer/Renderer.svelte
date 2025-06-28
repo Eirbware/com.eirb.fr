a4d4da1f2a90cf39c8b750e4b612b13186206f1b
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-06-28 14:50:53 +0200

    feat: first version of classical markdown renderer

--- components/src/renderer/Renderer.svelte
+++ components/src/renderer/Renderer.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
-import { preview } from "../runes/preview.svelte";
-    import Classic from "./Classic.svelte";
+  import { preview } from "../runes/preview.svelte";
+  import Classic from "./Classic.svelte";
+
+  // TODO: replace with the rendered markdown
+  const markdownContent = `The General Kenobi has said
+
+> *Hello **there**!*
+
+And I approve it`;
 </script>
 
 {#if preview.state}
   <!-- TODO: import Telegram -->
-  <Classic></Classic>
+  <Classic markdownContent="{markdownContent}"></Classic>
 {/if}
