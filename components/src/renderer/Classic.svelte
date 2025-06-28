48143811c597d65be1ec9427106177b8d27455e2
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-06-28 20:17:23 +0200

    fix: permissive linebreaking + refactor processings
    this time, the spoiler, the blockquoting and the permissive linebreaking
    are well-embedded (unless with the old library)

--- components/src/renderer/Classic.svelte
+++ components/src/renderer/Classic.svelte
@@ -1,44 +1,7 @@
 <script lang="ts">
-  import markdownit from "markdown-it";
-  import taskLists from "markdown-it-task-lists";
-  import highlightjs from "markdown-it-highlightjs";
-  import { spoiler } from "@mdit/plugin-spoiler";
-
   import "./css/classic-renderer.css";
 
-  const md = markdownit({
-    html: true, // for the <u> tag; TODO: sanitize with allowing <u> only
-    breaks: true,
-    linkify: true,
-  })
-    .use(taskLists)
-    .use(highlightjs)
-    .disable(["table"])
-    .use(spoiler);
-
-  const spoilerProcessing = {
-    pre: (mdContent: string) =>
-      mdContent
-        // ensure we do not have problem with existing %e% (not very probable)
-        .replace(/%/g, "+-%-+")
-        // we keep the original '!'s out of the analysis
-        .replace(/!/g, "%e%")
-        // we replace the '|'s by '!'s before the processing by the mdit
-        // spoiler plugin
-        .replace(/\|/g, "!"),
-    post: (htmlContent: string) =>
-      htmlContent
-        // The remaining '!'s are originally non-delimiter '|'s
-        .replace(/!/g, "|")
-        // Restore the original '!'s
-        .replace(/%e%/g, "!")
-        // Restore the original '%'s
-        .replace(/\+-%-\+/g, "%"),
-  };
-
-  function renderMarkdown(mdContent: string) {
-    return spoilerProcessing.post(md.render(spoilerProcessing.pre(mdContent)));
-  }
+  import { renderMarkdown } from "./markdown-to-message-html";
 
   export let markdownContent: string = "";
 </script>
---
+++ components/src/renderer/markdown-to-message-html.ts
@@ -0,0 +1,64 @@
+import markdownit from "markdown-it";
+import taskLists from "markdown-it-task-lists";
+import highlightjs from "markdown-it-highlightjs";
+import { spoiler } from "@mdit/plugin-spoiler";
+
+const md = markdownit({
+  html: true, // for the <u> tag; TODO: sanitize with allowing <u> only
+  breaks: true,
+  linkify: true,
+})
+  .use(taskLists)
+  .use(highlightjs)
+  .disable(["table"])
+  .use(spoiler);
+
+interface MarkdownRenderingCustomProcess {
+  pre: (mdContent: string) => string;
+  post: (htmlContent: string) => string;
+}
+
+const spoilerProcessing: MarkdownRenderingCustomProcess = {
+  pre: (mdContent: string) =>
+    mdContent
+      // ensure we do not have problem with existing %e% (not very probable)
+      .replace(/%/g, "+-%-+")
+      // we keep the original '!'s out of the analysis
+      .replace(/!/g, "%e%")
+      // we replace the '|'s by '!'s before the processing by the mdit
+      // spoiler plugin
+      .replace(/\|/g, "!"),
+  post: (htmlContent: string) =>
+    htmlContent
+      // The remaining '!'s are originally non-delimiter '|'s
+      .replace(/!/g, "|")
+      // Restore the original '!'s
+      .replace(/%e%/g, "!")
+      // Restore the original '%'s
+      .replace(/\+-%-\+/g, "%"),
+};
+
+const allowAdditionalLinebreaks: MarkdownRenderingCustomProcess = {
+  pre: (mdContent: string) =>
+    mdContent.replace(/\n{3,}/g, (match) => {
+      const extraBreaks = match.length - 2;
+      return "\n\n" + "<br/>\n".repeat(extraBreaks).trim();
+    }),
+  post: (htmlContent: string) => htmlContent,
+};
+
+const processes: MarkdownRenderingCustomProcess[] = [
+  spoilerProcessing,
+  allowAdditionalLinebreaks,
+];
+const pipe = (ps: ((s: string) => string)[]) => (s: string) =>
+  ps.reduce((acc, fn) => fn(acc), s);
+
+const preprocesses = pipe(processes.map((p) => p.pre));
+const postprocesses = pipe(processes.map((p) => p.post));
+
+export const renderMarkdown = pipe([
+  preprocesses,
+  (s) => md.render(s),
+  postprocesses,
+]);
