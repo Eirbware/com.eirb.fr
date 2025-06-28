0456db9b9c4b518651f80b478cbf632e5b5a4962
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-06-28 19:40:34 +0200

    feat: spoiler, small line-breaking and html allow

--- components/src/renderer/Classic.svelte
+++ components/src/renderer/Classic.svelte
@@ -1,11 +1,48 @@
 <script lang="ts">
   import markdownit from "markdown-it";
+  import taskLists from "markdown-it-task-lists";
+  import highlightjs from "markdown-it-highlightjs";
+  import { spoiler } from "@mdit/plugin-spoiler";
+
   import "./css/classic-renderer.css";
-  // TODO: add options
-  const md = markdownit();
+
+  const md = markdownit({
+    html: true, // for the <u> tag; TODO: sanitize with allowing <u> only
+    breaks: true,
+    linkify: true,
+  })
+    .use(taskLists)
+    .use(highlightjs)
+    .disable(["table"])
+    .use(spoiler);
+
+  const spoilerProcessing = {
+    pre: (mdContent: string) =>
+      mdContent
+        // ensure we do not have problem with existing %e% (not very probable)
+        .replace(/%/g, "+-%-+")
+        // we keep the original '!'s out of the analysis
+        .replace(/!/g, "%e%")
+        // we replace the '|'s by '!'s before the processing by the mdit
+        // spoiler plugin
+        .replace(/\|/g, "!"),
+    post: (htmlContent: string) =>
+      htmlContent
+        // The remaining '!'s are originally non-delimiter '|'s
+        .replace(/!/g, "|")
+        // Restore the original '!'s
+        .replace(/%e%/g, "!")
+        // Restore the original '%'s
+        .replace(/\+-%-\+/g, "%"),
+  };
+
+  function renderMarkdown(mdContent: string) {
+    return spoilerProcessing.post(md.render(spoilerProcessing.pre(mdContent)));
+  }
+
   export let markdownContent: string = "";
 </script>
 
 <div class="rendered-html">
-  {@html md.render(markdownContent)}
+  {@html renderMarkdown(markdownContent)}
 </div>
