faf825667d25eedaf52d52dcdaf19f2d90b07ee3
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-06-29 22:04:21 +0200

    fix: remove <p> tag in rendered html + linebreak
    this fix the issue of sulguk entity production in the flask
    server, which cause telegram messages to have unwanted linebreaks
    at the beginning of quote blocks or list item or code block etc.
    
    the line are also correctly broken in code blocks
    
    TODO: in blockquote, the lines are not broken for now

--- components/src/lib/markdown-to-message-html.ts
+++ components/src/lib/markdown-to-message-html.ts
@@ -18,6 +18,39 @@ interface MarkdownRenderingCustomProcess {
   post: (htmlContent: string) => string;
 }
 
+// --- disable <p> and replace <p/> by <br/> ---
+
+md.renderer.rules.paragraph_open = () => {
+  return "";
+};
+md.renderer.rules.paragraph_close = (tokens, idx) => {
+  if (tokens[idx + 1] && tokens[idx + 1].tag === "p") return "<br/>";
+  return "";
+};
+
+// --- disable <p> and replace <p/> by <br/> ---
+
+const allowAdditionalLinebreaks: MarkdownRenderingCustomProcess = {
+  pre: (mdContent: string) => {
+    // Transform 2+n linebreaks into 2 linebreaks and n+1 placeholders
+    // paragraphs, so they will be transformed into consecutive paragraph
+    // tags that will be transformed into <br/>
+    // enable this to lengthen the height space between two paragraphs
+    // as in message editors
+    const lineBreakPairToLineBreakTag = (s: string) =>
+      s.replace(
+        /\n{2,}/g,
+        (match) => "\n\n" + "\n\n%%%deadbeef%%%\n\n".repeat(match.length - 1),
+      );
+    return mdContent
+      .split("```")
+      .map((part, i) => (i % 2 ? part : lineBreakPairToLineBreakTag(part)))
+      .join("```");
+  },
+
+  post: (htmlContent: string) => htmlContent.replace(/%%%deadbeef%%%/g, ""),
+};
+
 const spoilerProcessing: MarkdownRenderingCustomProcess = {
   pre: (mdContent: string) =>
     mdContent
@@ -36,15 +69,6 @@ const spoilerProcessing: MarkdownRenderingCustomProcess = {
       .replace(/%e%/g, "!")
       // Restore the original '%'s
       .replace(/\+-%-\+/g, "%"),
-};
-
-const allowAdditionalLinebreaks: MarkdownRenderingCustomProcess = {
-  pre: (mdContent: string) =>
-    mdContent.replace(/\n{3,}/g, (match) => {
-      const extraBreaks = match.length - 2;
-      return "\n\n" + "<br/>".repeat(extraBreaks) + "\n\n";
-    }),
-  post: (htmlContent: string) => htmlContent,
 };
 
 const processes: MarkdownRenderingCustomProcess[] = [
