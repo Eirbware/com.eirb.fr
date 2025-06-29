384136e9c693a1e3cf307709f9aeca74f172db32
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-06-29 23:01:40 +0200

    fix: sanitized input and underline through plugin
    the underline is supported through a token replacement
    and the use of a plugin so the <u> tag can be used without
    the html tags to be allowed in the markdown-it rendering
    
    then, the input is now sanitized in the frontend

--- components/src/lib/markdown-to-message-html.ts
+++ components/src/lib/markdown-to-message-html.ts
@@ -1,13 +1,22 @@
 import markdownit from "markdown-it";
 import taskLists from "markdown-it-task-lists";
+import underline from "markdown-it-plugin-underline";
 import highlightjs from "markdown-it-highlightjs";
 import { spoiler } from "@mdit/plugin-spoiler";
 
+// TODO: this rendering is for telegram only
+
 const md = markdownit({
-  html: true, // for the <u> tag; TODO: sanitize with allowing <u> only
+  // the <u> tag is allowed through a replacement and a plugin
+  // with this option set to fault, the input is already sanitized
+  html: false,
+  // this option enable one-linebreak only (custom processing is set to add
+  // more linebreaks)
   breaks: true,
+  // Cool option
   linkify: true,
 })
+  .use(underline)
   .use(taskLists)
   .use(highlightjs)
   .disable(["table"])
@@ -51,11 +60,33 @@ const allowAdditionalLinebreaks: MarkdownRenderingCustomProcess = {
   post: (htmlContent: string) => htmlContent.replace(/%%%deadbeef%%%/g, ""),
 };
 
+// As we use %$special_token% special tokens for swapping some patterns,
+// we securize them with this process (even if their presence is not likely)
+const securizeSpecialTokens: MarkdownRenderingCustomProcess = {
+  pre: (mdContent) => mdContent.replace(/%/g, "£¤%£¤"),
+  post: (htmlContent) =>
+    htmlContent
+      // Restore the original '%'s
+      .replace(/£¤%£¤/g, "%"),
+};
+
+const underlineProcessing: MarkdownRenderingCustomProcess = {
+  pre: (mdContent) =>
+    mdContent
+      .replace(/\+/g, "%u%")
+      .replace(/<u>/g, "++")
+      .replace(/<\/u>/g, "++%%verydeadbeef%%"),
+  post: (htmlContent) =>
+    htmlContent
+      .replace(/\+\+%%verydeadbeef%%/g, "</u>")
+      .replace(/%%verydeadbeef%%/g, "") // Clean the remaining %%verydeadbeef%%
+      .replace(/\+\+/g, "<u>")
+      .replace(/%u%/g, "+"),
+};
+
 const spoilerProcessing: MarkdownRenderingCustomProcess = {
   pre: (mdContent: string) =>
     mdContent
-      // ensure we do not have problem with existing %e% (not very probable)
-      .replace(/%/g, "+-%-+")
       // we keep the original '!'s out of the analysis
       .replace(/!/g, "%e%")
       // we replace the '|'s by '!'s before the processing by the mdit
@@ -66,12 +97,12 @@ const spoilerProcessing: MarkdownRenderingCustomProcess = {
       // The remaining '!'s are originally non-delimiter '|'s
       .replace(/!/g, "|")
       // Restore the original '!'s
-      .replace(/%e%/g, "!")
-      // Restore the original '%'s
-      .replace(/\+-%-\+/g, "%"),
+      .replace(/%e%/g, "!"),
 };
 
 const processes: MarkdownRenderingCustomProcess[] = [
+  securizeSpecialTokens,
+  underlineProcessing,
   spoilerProcessing,
   allowAdditionalLinebreaks,
 ];
@@ -79,7 +110,7 @@ const pipe = (ps: ((s: string) => string)[]) => (s: string) =>
   ps.reduce((acc, fn) => fn(acc), s);
 
 const preprocesses = pipe(processes.map((p) => p.pre));
-const postprocesses = pipe(processes.map((p) => p.post));
+const postprocesses = pipe(processes.reverse().map((p) => p.post));
 
 export const renderMarkdown = pipe([
   preprocesses,
--- components/src/runes/html-rendered-content.svelte.ts
+++ components/src/runes/html-rendered-content.svelte.ts
@@ -2,5 +2,6 @@ import { renderMarkdown } from "../lib/markdown-to-message-html";
 import { markdownContent } from "./markdown-content.svelte";
 
 export function htmlRenderedContent() {
+  // TODO: its for telegram only for now
   return renderMarkdown(markdownContent.content);
 }
