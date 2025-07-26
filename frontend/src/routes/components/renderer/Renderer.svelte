b1f1b5464831751fd4a86a430abf11ad4e5f83d6
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-07-26 13:34:26 +0200

    feat: in progress telegram style imitation

--- frontend/src/routes/components/renderer/Renderer.svelte
+++ frontend/src/routes/components/renderer/Renderer.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import { preview } from "../runes/preview.svelte";
   import { htmlRenderedContent } from "../runes/html-rendered-content.svelte";
-  import Classic from "./Classic.svelte";
+	import Telegram from "./Telegram.svelte";
 </script>
 
 {#if preview.state}
-  <!-- TODO: import Telegram -->
-  <Classic htmlContent={htmlRenderedContent()}></Classic>
+  <Telegram htmlContent={htmlRenderedContent()}></Telegram>
 {/if}
--- frontend/src/routes/components/renderer/Telegram.svelte
+++ frontend/src/routes/components/renderer/Telegram.svelte
@@ -1,47 +1,68 @@
-<!-- Content for the second half -->
-<div class="page_body chat_page">
-  <div class="history">
-    <div class="message default clearfix">
-      <div class="pull_left userpic_wrap">
-        <div class="userpic userpic6" style="width: 42px; height: 42px">
-          <div class="initials" style="line-height: 42px"></div>
-        </div>
-      </div>
+<script lang="ts">
+	import './css/telegram-renderer.css';
+	import './css/telegram-new-renderer.css';
 
-      <div class="body">
-        <!-- TODO: add the automatic date -->
-        <div
-          class="pull_right date details"
-          title="14.04.2025 00:01:51 UTC+01:00"
-        >
-          00:01
-        </div>
+	import { browser } from '$app/environment';
 
-        <!-- TODO: add an default User -->
-        <div class="from_name">💚🩵Feufoll'eirb🩵💚</div>
+	let { htmlContent }: { htmlContent: string } = $props();
+</script>
 
-        <div class="text" id="rendered-content"></div>
-
-        <span class="reactions">
-          <span class="reaction">
-            <span class="emoji"> ❤ </span>
-
-            <span class="count"> 17 </span>
-          </span>
-
-          <span class="reaction">
-            <span class="emoji"> 🔥 </span>
-
-            <span class="count"> 4 </span>
-          </span>
-
-          <span class="reaction">
-            <span class="emoji"> 🎉 </span>
-
-            <span class="count"> 1 </span>
-          </span>
-        </span>
+<div class="telegram-bubble-content-wrapper">
+  <div class="telegram-bubble-content">
+    <div class="telegram-message-wrapper">
+      <div class="telegram-message">
+					{#if browser}
+						{@html htmlContent}
+					{/if}
       </div>
     </div>
   </div>
+</div>
+
+<div class="rendered-telegram">
+	<div class="page_body chat_page">
+		<div class="message default clearfix">
+			<div class="pull_left userpic_wrap">
+				<div class="userpic userpic6" style="width: 42px; height: 42px">
+					<div class="initials" style="line-height: 42px"></div>
+				</div>
+			</div>
+
+			<div class="body">
+				<!-- TODO: add the automatic date -->
+				<div class="pull_right date details" title="14.04.2025 00:01:51 UTC+01:00">00:01</div>
+
+				<!-- TODO: add an default User -->
+				<div class="from_name">💚🩵Feufoll'eirb🩵💚</div>
+
+				<!-- TODO: insert html here -->
+				<div class="text" id="rendered-content">
+					<!-- client-side rendering only (remove conflict with inexisting SSR) -->
+					{#if browser}
+						{@html htmlContent}
+					{/if}
+				</div>
+
+				<span class="reactions">
+					<span class="reaction">
+						<span class="emoji"> ❤ </span>
+
+						<span class="count"> 17 </span>
+					</span>
+
+					<span class="reaction">
+						<span class="emoji"> 🔥 </span>
+
+						<span class="count"> 4 </span>
+					</span>
+
+					<span class="reaction">
+						<span class="emoji"> 🎉 </span>
+
+						<span class="count"> 1 </span>
+					</span>
+				</span>
+			</div>
+		</div>
+	</div>
 </div>
