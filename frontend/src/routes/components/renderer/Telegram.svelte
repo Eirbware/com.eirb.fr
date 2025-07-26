6396bc002515fb33b1d25c1c6f4c7b37329ef9a0
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-07-26 17:46:53 +0200

    feat: code blocks with telegram ui

--- frontend/src/routes/components/renderer/Telegram.svelte
+++ frontend/src/routes/components/renderer/Telegram.svelte
@@ -1,22 +1,53 @@
 <script lang="ts">
+	import 'highlight.js/styles/default.css';
 	import './css/telegram-renderer.css';
 	import './css/telegram-new-renderer.css';
 
 	import { browser } from '$app/environment';
 
 	let { htmlContent }: { htmlContent: string } = $props();
+	function renderSulgukCaveats(content: string) {
+		/* Edit the HTML to add newlines around the code blocks, which is forced by
+		 * the sulguk intergration in the backend.
+		 */
+		// TODO:
+		return content;
+	}
+	function renderCodeHeader(content: string) {
+		const parser = new DOMParser();
+		const rendered = parser.parseFromString(content, 'text/html');
+		const codeHeader = (language: string) => {
+			const newElt = rendered.createElement('div');
+			newElt.classList.add('code-header');
+			newElt.innerHTML = `<span class="code-header-name">${language.charAt(0).toUpperCase() + language.slice(1)}</span>
+<span class="code-header-button"></span>
+<span class="code-header-button"></span>`;
+			return newElt;
+		};
+		rendered.querySelectorAll('pre').forEach((preElt) => {
+			const codeBlock = preElt.querySelector('code');
+			if (codeBlock === null) return;
+			const codeBlockLanguage = codeBlock.classList[1].split('language-')[1];
+			preElt.insertBefore(codeHeader(codeBlockLanguage), codeBlock);
+		});
+		return rendered.documentElement.outerHTML;
+	}
+
+	function htmlToDisplayedHTML(content: string) {
+		return renderSulgukCaveats(renderCodeHeader(content));
+	}
 </script>
 
 <div class="telegram-bubble-content-wrapper">
-  <div class="telegram-bubble-content">
-    <div class="telegram-message-wrapper">
-      <div class="telegram-message">
-					{#if browser}
-						{@html htmlContent}
-					{/if}
-      </div>
-    </div>
-  </div>
+	<div class="telegram-bubble-content">
+		<div class="telegram-message-wrapper">
+			<div class="telegram-message">
+				{#if browser}
+					{@html htmlToDisplayedHTML(htmlContent)}
+				{/if}
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="rendered-telegram">
